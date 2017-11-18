1bcb983fa74e0ab2b002bb1f771f454aa243a014
Mainex <mainex@localhost.localdomain>
2017-11-18 09:58:40 +0000

    first commit

---
+++ myprofile.sh
@@ -0,0 +1,11 @@
+alias v='vim'
+alias b='cd ..'
+alias notes='cd ~/workdir/notes'
+alias share='sudo mount -t vboxsf centos ~/share'
+alias rc='v  ~/workdir/scripts/myprofile.sh'
+alias l='ls -ltra'
+alias vrc='v ~/.vimrc'
+alias trace='cat /sys/kernel/debug/tracing/trace | head -10; cat /sys/kernel/debug/tracing/trace | tail -50'
+alias linuxcode='cd /home/mainex/rpmbuild/SOURCES/linux-3.10.0-693.5.2.el7'
+
+export HISEFILESIZE=10000
