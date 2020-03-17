e07fd5ae2ef66553bb0a90c429292f7004fa08a0
t20x <asdas>
2020-03-17 11:57:14 +0000

    asdf

--- myprofile.sh
+++ myprofile.sh
@@ -1,12 +1,22 @@
 alias v='vim'
 alias b='cd ..'
 alias notes='cd ~/workdir/notes'
-#alias share='sudo mount -t vboxsf centos ~/share'
+alias m='sudo mount -t vboxsf exchange /media/sf_exchange/; sudo mount -t vboxsf shared /media/sf_shared/'
 alias share='cd /media/sf_exchange'
 alias rc='v  ~/workdir/scripts/myprofile.sh'
 alias l='ls -ltra'
 alias vrc='v ~/.vimrc'
 alias trace='cat /sys/kernel/debug/tracing/trace | head -10; cat /sys/kernel/debug/tracing/trace | tail -50'
 alias linuxcode='cd /home/mainex/rpmbuild/SOURCES/linux-3.10.0-693.5.2.el7'
+alias estd='cd /usr/local/include/c++/10.0.0' 
+alias std='cd /media/sf_shared/g/gcc/libstdc++-v3/include'
+alias fast='cd /media/sf_shared/'
+alias python='python3.7'
 
+export BOOST_INCLUDE=/media/sf_shared/boost_1_71_0
+export PROJECTS=~/cpp_snippets
+export PATH=/usr/local/bin:/home/maine/fasm:$PATH
+export LD_LIBRARY_PATH=/usr/local/lib64:$LD_LIBRARY_PATH
+
+export INCLUDE=/home/maine/fasm/source/Linux 
 export HISEFILESIZE=10000
